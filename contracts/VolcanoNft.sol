3ae595dc34a6fbf74d12455261dc0c1f0c160f38
Pierre Guerineau <polemoflora@gmail.com>
2022-10-31 20:51:49 -0400

    contract_v2

--- contracts/VolcanoNft.sol
+++ contracts/VolcanoNft.sol
@@ -2,10 +2,27 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract VolcanoNFT is ERC721 {
+contract VolcanoNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
     {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return
+            "https://drive.google.com/file/d/1Jwk5zilMV8_RDygEB2dDPBUtx1VU6882/view?usp=share_link";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
 }
